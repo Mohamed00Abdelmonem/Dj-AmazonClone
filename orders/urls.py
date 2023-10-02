e0d3c47d17878cd2ffeb5bf9a6253b27008e66ad
unknown <mmohamedabdelm@gmail.com>
2023-10-02 10:03:54 +0300

    add checkout page p1

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import OrderList
+from .views import OrderList, checkout
 
 
 urlpatterns = [
-    path('', OrderList.as_view())
+    path('', OrderList.as_view()),
+    path('checkout', checkout)
+
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,7 @@
-from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Order
+from .models import Order, Cart, CartDetail, Coupon
 # Create your views here.
 
 
@@ -14,3 +13,11 @@ class OrderList(ListView):
     def get_queryset(self):
         queryset = super().get_queryset().filter(user=self.request.user)
         return queryset
+    
+
+
+def checkout(request):
+    cart = Cart.objects.get(user=request.user, status='InProgress')
+    cart_detail = CartDetail.objects.filter(cart=cart)
+
+    return render(request, 'orders/checkout.html', {'cart_detail':cart_detail})    
