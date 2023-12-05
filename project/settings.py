b3df730b3bf61d8d748ec036c56a6957926a775d
unknown <mmohamedabdelm@gmail.com>
2023-12-05 15:35:43 +0200

    add celery in docker compose

--- project/settings.py
+++ project/settings.py
@@ -225,6 +225,10 @@ LANGUAGES = [
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
+CELERY_BROKER_URL = 'redis://myredis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://myredis:6379/0'
+
+
 
 # CELERY_BROKER_URL = 'redis://myredis:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://myredis:6379/0'
