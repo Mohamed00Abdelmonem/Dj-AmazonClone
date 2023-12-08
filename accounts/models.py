d774f431f8dc9ac9cb3a915659e7868f8aae11c8
unknown <mmohamedabdelm@gmail.com>
2023-12-08 13:17:16 +0200

    add design user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from utils.generate_code import generate_code
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile')
+    code = models.CharField(max_length=10, default=generate_code)
+
+
+
+
+
+ADRESS_TYPE = (
+    ('Home','Home' ),
+    ('Bussiness', 'Bussiness'),
+    ('Academy','Academy' ),
+    ('Office','Office' ),
+    ('Others','Others' ),
+)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, related_name='user_address', on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=ADRESS_TYPE)
+    address = models.TextField(max_length=300)
+    notes = models.TextField(max_length=400, null=True, blank=True)
+
+
+
+
+
+
+PHONE_TYPE = (
+    ('Primary','Primary' ),
+    ('Secondry', 'Secondry'),
+    ('Academy','Academy' ),
+    ('Office','Office' ),
+    ('Others','Others' ),
+)
+
+
+class Phone(models.Model):
+    user = models.ForeignKey(User, related_name='user_phone', on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=PHONE_TYPE)
+    phone = models.CharField(max_length=25)
