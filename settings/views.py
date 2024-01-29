aa87839ec10315f114d5e84e6c50a561a5e10dfe
unknown <mmohamedabdelm@gmail.com>
2024-01-29 13:49:11 +0200

    Finally committing changes to fix an issue in _request_context_bind_template. Wrote this commit in English for improved clarity.

--- settings/views.py
+++ settings/views.py
@@ -6,19 +6,24 @@ from django.views.decorators.cache import cache_page
 # Create your views here.
 
 
-# @cache_page(60 * 3) # 3 minutes
+# @cache_page(60 * 3) # 3 minutes                                                                                                                                                                           
 def home(request):
-    brands = Brand.objects.all().annotate(brand_count=Count('product_brand'))
-    sale_products = Product.objects.filter(flag='sale')[:10] 
-    new_products = Product.objects.filter(flag='new')[:6] 
-    feature_products = Product.objects.filter(flag='feature')[:5]
-    reviwes = Review.objects.all()[:10] 
+       brands = Brand.objects.all().annotate(brand_count=Count('product_brand'))
+       sale_products = Product.objects.filter(flag='sale')[:10] 
+       new_products = Product.objects.filter(flag='new')[:6] 
+       feature_products = Product.objects.filter(flag='feature')[:5]
+       reviwes = Review.objects.all()[:10] 
 
+       print("brands:", brands)
+       print("sale_products:", sale_products)
+       print("new_products:", new_products)
+       print("feature_products:", feature_products)
+       print("reviwes:", reviwes)
 
-    return render(request, 'settings/home.html',{
-                                                'brands':brands , 
-                                                'sale_products':sale_products, 
-                                                'new_products':new_products, 
-                                                'feature_products':feature_products, 
-                                                'reviews':reviwes
-                                                 })
+       return render(request, 'settings/home.html', {
+           'brands': brands,
+           'sale_products': sale_products,
+           'new_products': new_products,
+           'feature_products': feature_products,
+           'reviews': reviwes
+       })
