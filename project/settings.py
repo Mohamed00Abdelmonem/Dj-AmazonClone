83ebca6b5a323cc5e6773cc69ed5bf383cd61d35
unknown <mmohamedabdelm@gmail.com>
2024-03-28 21:25:41 +0200

    update some code in fronetend

--- project/settings.py
+++ project/settings.py
@@ -292,9 +292,10 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mmohamedabdelm@gmail.com'  # Replace with your Gmail email address
-EMAIL_HOST_PASSWORD = 'edkwbyqfithzanxw'  # Replace with your Gmail app password
+EMAIL_HOST_PASSWORD = 'gttk wiij ttju ioyk'  # Replace with your Gmail app password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 
 
 
