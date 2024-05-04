ce5dc478cc411a8a13a02e7f2ca41ce95008aa8e
unknown <mmohamedabdelm@gmail.com>
2024-05-04 23:55:31 +0300

    update for deployed

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
+CSRF_TRUSTED_ORIGINS = ['https://dj-amazonclone-production.up.railway.app/']
 
 
 # Application definition
