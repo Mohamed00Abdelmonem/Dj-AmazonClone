43689313853dceb689549f8b5891bec3d2ab1ae5
unknown <mmohamedabdelm@gmail.com>
2024-06-10 07:55:45 +0300

    fix dummy_data

--- project/settings.py
+++ project/settings.py
@@ -220,7 +220,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / "static" ]
